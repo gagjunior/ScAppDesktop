1e6911c3803c0ea9531d2b4b91ea7f43828a6d27
gagjunior <48300739+gagjunior@users.noreply.github.com>
2020-05-21 17:44:16 -0300

    inclusão de telas

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import kivy
 from kivy.app import App
-from telas.views import Login, Cadastro, Gerenciador
+from telas.views import Gerenciador
 
 kivy.require('1.11.1')
-
 
 
 class ScApp(App):
     def build(self):
         self.title = 'Relatório de Serviço'
         return Gerenciador()
-
-
 
 
 if __name__ == "__main__":
--- telas/views.py
+++ telas/views.py
@@ -1,28 +1,58 @@
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
+from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition, SlideTransition
 from kivy.core.window import Window
+from kivy.clock import Clock
 import os
 
 dir_name = os.path.dirname(__file__)
 
+# Tela Splash
+Builder.load_string(open(f'{dir_name}/splash.kv', encoding='utf-8').read())
+class Splash(Screen):
+    def tela_login(self, *args):
+        self.manager.transition = FadeTransition()
+        self.manager.current = 'login'
+        
+    def on_enter(self, *args):
+        Clock.schedule_once(self.tela_login, 2)
+        print ('Aguarde...')     
+    
+
+# Tela Login
 Builder.load_string(open(f'{dir_name}/login.kv', encoding='utf-8').read())
 class Login(Screen):
     def tela_cadastro(self):
-        self.manager.transition = FadeTransition()
-        self.manager.current = 'cadastro'    
+        self.manager.transition = SlideTransition()      
+        self.manager.current = 'cadastro'
+
+    def tela_menu(self):
+        pass  
 
 
+# Tela Cadastro
 Builder.load_string(open(f'{dir_name}/cadastro.kv', encoding='utf-8').read())
 class Cadastro(Screen):
     def on_pre_enter(self, *args):
         Window.set_title('ScApp - Cadastro de Usuário')
+    
+    def tela_login(self):
+        self.manager.transition = SlideTransition()
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login'
+
+class MenuInicio(Screen):
+    pass
 
 
+# Gerenciador de Telas
 class Gerenciador(ScreenManager):
     def __init__(self, **kwargs):
         super(Gerenciador, self).__init__(**kwargs)
+        self.add_widget(Splash(name='splash'))
         self.add_widget(Login(name='login'))
-        self.add_widget(Cadastro(name='cadastro'))  
+        self.add_widget(Cadastro(name='cadastro'))    
+        
+
 
 
 
